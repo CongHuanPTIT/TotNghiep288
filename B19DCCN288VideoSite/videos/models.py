1ed803a5fd364ef0b8ee32a08dd558926fe44ef9
CongHuanNguyen <nguyenconghuan12i.lmnx@gmail.com>
2023-10-16 08:14:02 +0700

    Giao diện màn tạo video
    Cập nhật thêm...

---
+++ B19DCCN288VideoSite/videos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+from django.core.validators import FileExtensionValidator
+
+# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    video_file = models.FileField(upload_to='uploads/video_files', validators = [FileExtensionValidator(allowed_extensions=['mp4', 'mkv'])])
+    thumbnail = models.FileField('uploads/thumbnails', validators = [FileExtensionValidator(allowed_extensions=['png', 'jpeg', 'jpg'])])
+    date_posted = models.DateTimeField(default=timezone.now)
+
